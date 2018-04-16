d45f0124b0a73df019baef789d9a7d6329d009f4
Hadi <hadi.effatian@gmail.com>
2018-04-16 15:54:55 +0200

    Created project

---
+++ src/main/java/at/refugeescode/mp13_the_magic_8_ball/Mp13TheMagic8BallApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp13_the_magic_8_ball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp13TheMagic8BallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp13TheMagic8BallApplication.class, args);
+	}
+}
